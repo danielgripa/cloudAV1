36791c72b62c4a765d12a21badf48188f1e4a9ee
danielgripa <daniel.gripa@gmail.com>
2024-09-30 19:07:55 -0300

    projeto spring base

---
+++ src/main/java/br/edu/ibmec/demo/CloudComputingAv1Application.java
@@ -0,0 +1,13 @@
+package br.edu.ibmec.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudComputingAv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudComputingAv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ibmec/demo/CloudComputingAv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ibmec.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudComputingAv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
