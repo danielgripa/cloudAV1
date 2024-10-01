06420e6dbb39e6bd678110259344ec25a4ab244b
danielgripa <112982758+danielgripa@users.noreply.github.com>
2024-10-01 18:22:00 -0300

    Update CloudComputingAv1Application.java
    debug

--- src/main/java/br/edu/ibmec/demo/CloudComputingAv1Application.java
+++ src/main/java/br/edu/ibmec/demo/CloudComputingAv1Application.java
@@ -1,13 +1,14 @@
-package br.edu.ibmec.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CloudComputingAv1Application {
-	public static void main(String[] args) {
-	    System.out.println("DB User: " + System.getenv("DB_USERNAME"));
-	    System.out.println("DB Password: " + System.getenv("DB_PASSWORD"));
-	    SpringApplication.run(Application.class, args);
-	}
+
+    public static void main(String[] args) {
+        // Prints para debug
+        System.out.println("DB User: " + System.getenv("DB_USERNAME"));
+        System.out.println("DB Password: " + System.getenv("DB_PASSWORD"));
+
+        SpringApplication.run(CloudComputingAv1Application.class, args);
+    }
 }
